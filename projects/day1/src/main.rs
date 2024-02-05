f9794eae5ee0c677e212cc11fe9f6df5d265f540
phelan-j <48454903+phelan-j@users.noreply.github.com>
2024-02-05 17:49:50 +0000

    Updated to do in one pass of string - TODO: speed up by continuing forward search

--- projects/day1/src/main.rs
+++ projects/day1/src/main.rs
@@ -13,7 +13,7 @@ where P: AsRef<Path>, {
 
 fn main() {
     let mut sum = 0;
-    if let Ok(lines) = read_lines("./inputs/test_input.txt") {
+    if let Ok(lines) = read_lines("./inputs/input.txt") {
         for line in lines.flatten() {
             let calib_value = calibration_value(&line);
             sum = sum + calib_value;
@@ -22,12 +22,8 @@ fn main() {
     println!("{sum}");
 }
 
-fn calibration_value(value: &String) -> u32 {
-    let first_digit = first_number(&value);
-    let last_digit = last_number(&value);
-    10 * first_digit + last_digit
-}
-
+// returns the numeric value of a single char numeric string
+// or None otherwise
 fn as_numeric_string(value: &str) -> Option<u32> {
     if value.len() == 1 {
         if let Some(c) = value.chars().next() {
@@ -37,48 +33,64 @@ fn as_numeric_string(value: &str) -> Option<u32> {
     None
 }
 
-fn first_number(value: &String) -> u32 {
-    let n = value.len();
-    for i in 0..n {
-        let chr_str = &value[i..=i];
-        if let Some(p) = as_numeric_string(chr_str) {
-            return p
+// checks if a digit string is contained in value at index
+// if this would be past the length of the string returns false
+fn check_digit_string(value: &String, index: usize, digit: &str) -> bool {
+    if digit.len() <= value.len() - index {
+        let digit_str = &value[index..index+digit.len()];
+        if digit == digit_str {
+            return true
         }
-        else {
+    }
+    false
+}
+
+// for a string calculates the number which has 
+// the first digit as the first numeric or verbal
+// number representation and the last digit as the 
+// last numeric or verbal number representation in 
+// the string
+fn calibration_value(value: &String) -> u32 {
+    let n = value.len();
+    let mut found_forwards = false;
+    let mut found_backwards = false;
+    let mut f = 0;
+    let mut b = 0;
+
+
+    let i_max = n;
+    for i in 0..i_max {
+        let r = n - i - 1;
+        if !found_forwards {
+            // TODO: keep forward search if not found backwards string
+            if let Some(p) = as_numeric_string(&value[i..=i]) {
+                found_forwards = true;
+                f = p;
+            }
+        }
+        if !found_backwards {
+            if let Some(p) = as_numeric_string(&value[r..=r]) {
+                found_backwards = true;
+                b = p;
+            }
+        }
+        if !found_forwards || !found_backwards {
             for j in 0..DIGITS.len() {
                 let digit = DIGITS[j];
-                if digit.len() <= n - i {
-                    let digit_str = &value[i..i+digit.len()];
-                    if digit == digit_str {
-                        return j as u32
-                    }
+                if !found_forwards && check_digit_string(value, i, digit) {
+                    found_forwards = true;
+                    f = j as u32;
+                }
+                if !found_backwards && check_digit_string(value, r, digit) {
+                    found_backwards = true;
+                    b = j as u32;
+                }
+                if found_forwards && found_backwards {
+                    break;
                 }
             }
         }
     }
-    0
+    10 * f + b
 }
 
-
-fn last_number(value: &String) -> u32 {
-    let n = value.len();
-    for r in 0..n {
-        let i = n - r - 1;
-        let chr_str = &value[i..=i];
-        if let Some(p) = as_numeric_string(chr_str) {
-            return p
-        }
-        else {
-            for j in 0..DIGITS.len() {
-                let digit = DIGITS[j];
-                if digit.len() <= n - i {
-                    let digit_str = &value[i..i+digit.len()];
-                    if digit == digit_str {
-                        return j as u32
-                    }
-                }
-            }
-        }
-    }
-    0
-}
