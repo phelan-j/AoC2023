035388b77135c827130a0e7000509d430f0d5e7d
phelan-j <48454903+phelan-j@users.noreply.github.com>
2024-02-26 08:21:16 +0000

    Added working soluton for part two

--- projects/day4/src/main.rs
+++ projects/day4/src/main.rs
@@ -1,12 +1,31 @@
 use day4::*;
 use std::collections::HashSet;
+use std::collections::LinkedList;
 fn main() {
-    if let Ok(lines) = read_lines("./input/test_input.txt") {
+    if let Ok(lines) = read_lines("./input/input.txt") {
         let mut sum = 0;
         let mut winning: HashSet<u32> = HashSet::new();
+        let mut mult_list: LinkedList<u32> = LinkedList::new();
         for line in lines.flatten() {
-            let card_val = calculate_scratch_card_points(&line, &mut winning);
-            sum += card_val;
+            // part one
+            //let card_val = calculate_scratch_card_points(&line, &mut winning);
+            //sum += card_val;
+            
+            // part two
+            let mut matches = calculate_scratch_card_matches(&line, &mut winning);
+            let count = if let Some(first) = mult_list.pop_front() { first + 1 } else { 1 };
+            sum = sum + count;
+
+            if matches > 0 { 
+                for item in mult_list.iter_mut() {
+                    *item += count;
+                    matches -= 1;
+                    if matches <= 0 { break; }
+                }
+                for _i in 0..matches {
+                    mult_list.push_back(count);
+                }
+            }
         }
         println!("{sum}");
     }
@@ -38,6 +57,7 @@ fn calculate_scratch_card_matches(line: &str, winning: &mut HashSet<u32>) -> u32
             val = 0;
         }
     }
+    if val > 0 { winning.insert(val); }
     while let Some(c) = chars.next() {
         if let Some(d) = c.to_digit(10) { val = 10 * val + d; }
         else {
@@ -45,6 +65,6 @@ fn calculate_scratch_card_matches(line: &str, winning: &mut HashSet<u32>) -> u32
             val = 0;
         }
     }
-    if winning.contains(&val) { match_count += 1; }
+    if val > 0 && winning.contains(&val) { match_count += 1; }
     match_count
 }
