39840b7529f95d47ce342f2d4a0e01483f6811c3
phelan-j <48454903+phelan-j@users.noreply.github.com>
2024-03-06 17:33:03 +0000

    Added working solution for day 6 part two

--- projects/day6/src/lib.rs
+++ projects/day6/src/lib.rs
@@ -29,16 +29,37 @@ pub fn parse_to_vec(line: &str) -> Vec<u32> {
     vec
 }
 
-pub fn part_one(times: Vec<u32>, distances: Vec<u32>) -> u32 {
-    let mut prd = 1;
+pub fn parse_ignore_space(line: &str) -> f64
+{
+    let mut val = None;
+    let mut chars = line.chars();
+    while let Some(c) = chars.next() {
+        if let Some(d) = c.to_digit(10) {
+            let d = d as f64;
+            val = match val {
+                Some(v) => Some(10.0 * v + d),
+                None => Some(d)
+            }
+        }
+    }
+    match val {
+        Some(v) => v,
+        None => 0.0
+    }
+}
+
+
+
+
+pub fn part_one(times: Vec<f64>, distances: Vec<f64>) -> f64 {
+    let mut prd : f64 = 1.0;
     for i in 0..times.len() {
-        let n = times[i];
-        let r = distances[i];
-        let x = ((n * n - 4 * r) as f64).sqrt();
-        let x = 0.5 * (n as f64 + x);
-        let x = 2 * (x.ceil() as u32) - n - 1;
+        let x = part_two(times[i], distances[i]);
         prd *= x;
     }
     prd
 }
 
+pub fn part_two(n: f64, r: f64) -> f64 {
+    2.0 * (0.5 * (n + (n * n - 4.0 * r).sqrt())).ceil() - n - 1.0
+}
--- projects/day6/src/main.rs
+++ projects/day6/src/main.rs
@@ -2,9 +2,13 @@ use day6::*;
 fn main() { 
     if let Ok(lines) = read_lines("./input/input.txt") {
         let mut flattened = lines.flatten();
-        let times = parse_to_vec(&flattened.next().unwrap());
-        let distances = parse_to_vec(&flattened.next().unwrap());
-        let answer = part_one(times, distances);
+        //let times = parse_to_vec(&flattened.next().unwrap());
+        //let distances = parse_to_vec(&flattened.next().unwrap());
+        //let answer = part_one(times, distances);
+        //
+        let time = parse_ignore_space(&flattened.next().unwrap());
+        let distance = parse_ignore_space(&flattened.next().unwrap());
+        let answer = part_two(time, distance);
         println!("{answer}");
     }
 }
