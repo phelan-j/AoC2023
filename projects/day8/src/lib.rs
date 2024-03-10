1a2726d5294d2c5c91d695e909ec2c3d19c67bbf
phelan-j <48454903+phelan-j@users.noreply.github.com>
2024-03-10 18:13:30 +0000

    Updated methodology to allow for maps with any number of nodes (not just two). Also added naive solution for day two which will not terminate.

--- projects/day8/src/lib.rs
+++ projects/day8/src/lib.rs
@@ -1,54 +1,73 @@
 use std::collections::HashMap;
 
 const ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const ALPHABET_LENGTH : usize = ALPHABET.len() as usize;
 const NODE_LABEL_LENGTH : usize = 3;
-const START : u32 = 0;
-const END : u32 = (ALPHABET.len() as u32).pow(NODE_LABEL_LENGTH as u32) - 1;
+const START : usize = 0;
+const END : usize = ALPHABET_LENGTH.pow(NODE_LABEL_LENGTH as u32) - 1;
 
-pub fn part_one<I>(lines: I) -> u32
+pub fn part_one<I>(lines: I) -> u64
     where I : Iterator<Item = String> {
-    node_path_count::<I>(lines)
+    node_path_count::<I>(lines, |x| x == START, |x| x == END)
 }
 
-pub fn node_path_count<I>(mut lines: I) -> u32
+pub fn part_two<I>(lines: I) -> u64
     where I : Iterator<Item = String> {
-    let mut nodes : HashMap<u32,(u32,u32)> = HashMap::new();
+    node_path_count::<I>(lines, |x| x % ALPHABET_LENGTH == START, |x| x % ALPHABET_LENGTH == ALPHABET_LENGTH - 1)
+}
+
+
+pub fn node_path_count<I>(mut lines: I, is_start: fn(usize) -> bool, is_end: fn(usize) -> bool) -> u64
+    where I : Iterator<Item = String> {
+    let mut nodes : HashMap<usize,Vec<usize>> = HashMap::new();
     let line = lines.next().unwrap();
-    let left: Vec<bool> = line.chars().map(|c| match c { 
-        'L' => true, 
-        'R' => false, 
+    let child: Vec<usize> = line.chars().map(|c| match c { 
+        'L' => 0usize, 
+        'R' => 1usize, 
         c => panic!("unknown instruction {c}") 
     }).collect();
     // skip
     lines.next();
+    let mut paths : Vec<usize> = Vec::new();
     for line in lines {
-        let (k,l,r) = parse_node_line(&line);
-        nodes.insert(k, (l,r));
+        let vec = parse_node_line(&line);
+        let k = vec[0];
+        nodes.insert(k, vec[1..].to_vec()); 
+        if is_start(k) {
+            paths.push(k);
+        }
     }
-    let mut i : u32 = 0;
-    let mut k = START;
-    while k != END {
-        let d = left[i as usize % left.len()];
-        let v = nodes[&k];
-        k = if d { v.0 } else { v.1 };
-        i += 1
+    let mut i : u64 = 0;
+    loop {
+        let j = (i % (child.len() as u64)) as usize;
+        let d = child[j];
+        let mut b = true;
+        for p in &mut paths {
+            *p = nodes[p][d];
+            b = b && is_end(*p);
+        }
+        i += 1;
+        if b { break; }
     }
     i
 }
 
 
-fn parse_node_line(line: &str) -> (u32, u32, u32) {
-    let a = parse_node(&line[0..NODE_LABEL_LENGTH]);
-    let b = parse_node(&line[NODE_LABEL_LENGTH + 4..2 * NODE_LABEL_LENGTH + 4]);
-    let c = parse_node(&line[2 * NODE_LABEL_LENGTH + 6..3 * NODE_LABEL_LENGTH + 6]);
-    (a,b,c)
-}
-
-fn parse_node(s: &str) -> u32 {
-    let mut v : u32 = 0;
-    let chars = s.chars();
-    for c in chars {
-        v = v * ALPHABET.len() as u32 + ALPHABET.find(c).unwrap() as u32;
+fn parse_node_line(line: &str) -> Vec<usize> {
+    let mut vec : Vec<usize> = Vec::new();
+    let mut val : Option<usize> = None;
+    for c in line.chars() {
+        if let Some(i) = ALPHABET.find(c) {
+            val = Some(match val {
+                Some(v) => v * ALPHABET_LENGTH + i,
+                None => i
+            });
+        }
+        else if let Some(v) = val {
+            vec.push(v);
+            val = None;
+        }
     }
-    v
+    if let Some(v) = val { vec.push(v); }
+    vec
 }
--- projects/day8/src/main.rs
+++ projects/day8/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     if let Ok(lines) = read_lines("./input/input.txt") {
         let flattened = lines.flatten();
         let answer = part_one(flattened);
+        //let answer = part_two(flattened);
         println!("{answer}");
     }
 }
