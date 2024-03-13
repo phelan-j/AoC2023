2b151c526d9e3fcf96f466e6520c7296f5e72b50
phelan-j <48454903+phelan-j@users.noreply.github.com>
2024-03-13 21:28:21 +0000

    Added working solution for day 9 part two

--- projects/day9/src/lib.rs
+++ projects/day9/src/lib.rs
@@ -2,40 +2,54 @@ use utils::*;
 
 pub fn part_one<I>(lines: I) -> i64
     where I : Iterator<Item = String> {
-    next_in_sequence::<I>(lines)
+    next_in_sequence::<I>(lines, 0, |m, n| n - m + 1 as i64)
 }
 
-pub fn next_in_sequence<I>(lines: I) -> i64
+pub fn part_two<I>(lines: I) -> i64
+    where I : Iterator<Item = String> {
+    next_in_sequence::<I>(lines, 1, |_,_| 0)
+}
+
+
+pub fn next_in_sequence<I>(lines: I, s: i64, t: fn(i64, i64) -> i64) -> i64
     where I : Iterator<Item = String> {
     let mut sum = 0;
     for line in lines {
-        let v = custom_parse_to_vec::<i64>(&line, " ", |x| x.parse::<i64>().expect("error"));
-        let mut b_p = 1;
-        for i in 1..v.len() {
-            let b = diff_sum(&v, v.len() - i - 1, i);
-            if b == 0 && b_p == 0 {
-                let n = (-1i64).pow(i as u32) * diff_sum(&v, v.len() - i + 1, i - 1);
-                sum += n;
-                break;
-            }
-            b_p = b;
-        }
-        if b_p != 0 {
-            let n = diff_sum(&v, 0, v.len());
-            sum += n;
-        }
+        let a = custom_parse_to_vec::<i64>(&line, " ", |x| x.parse::<i64>().expect("error"));
+        let m = get_sequence_depth(&a);
+        let n = a.len() as i64;
+        let o = t(m,n);
+        let v = (-1i64).pow(s as u32) * binomial_sum(&a, s, s + m - 2, o, m - 1);
+        sum += v;
     }
     sum
 }
 
-fn diff_sum(vec: &Vec<i64>, o: usize, n: usize) -> i64 {
+// calculate number of diff sequences needed to find 0 difference
+fn get_sequence_depth(a: &Vec<i64>) -> i64 {
+    let n = (a.len() - 1) as i64;
+    let mut b_p = 1;
+    for m in 1..n {
+        let b = binomial_sum(&a, 0, m, n - m - 1, m);
+        if b == 0 && b_p == 0 {
+            return m
+        }
+    }
+    n
+}
+
+// given a sequence a_i
+// calculate the sum from k = k_start to k_end 
+// of (-1)^k (n choose k) a_{k - k_offset}
+fn binomial_sum(a: &Vec<i64>, k_start: i64, k_end: i64, i_start: i64, n: i64) -> i64 {
     let mut sum = 0;
-    let k_max = if o + n < vec.len() - 1 { n } else { vec.len() - 1 - o };
-    for k in 0..=k_max {
+    for k in k_start..=k_end {
+        let i = i_start + k - k_start;
         let s = (-1i64).pow(k as u32);
-        let b = choose(n as i64, k as i64);
-        let v = vec[o + k];
-        sum += s * b * v;
+        let b = choose(n, k);
+        let a_i = a[i as usize];
+        let s_i = s * b * a_i;
+        sum += s_i;
     }
     sum
 }
