bca707f6bfd4188ba5a18b6efdbd250e564324d4
phelan-j <48454903+phelan-j@users.noreply.github.com>
2024-03-16 20:57:43 +0000

    Added working solution for day 10 part two

--- projects/day10/src/lib.rs
+++ projects/day10/src/lib.rs
@@ -2,37 +2,30 @@ const N : u8 = 0b0001;
 const E : u8 = 0b0010;
 const S : u8 = 0b0100;
 const W : u8 = 0b1000;
-const NE : u8 = N | E;
-const NW : u8 = N | W;
-const SE : u8 = S | E;
-const SW : u8 = S | W;
-const NS : u8 = N | S;
-const WE : u8 = W | E;
 const MASK : u8 = N | E | S | W;
-
-
 const DIRECTION : &str = ".NELS|FXWJ-X7";
 
-fn debug_print(v: u8) -> String  {
-    let v = v as usize;
-    DIRECTION[v..=v].to_string()
+pub fn part_one<I>(lines: I) -> i64
+    where I : Iterator<Item = String> {
+    calculate_path_and_closure::<I>(lines).0
+}
+        
+pub fn part_two<I>(lines: I) -> i64
+    where I : Iterator<Item = String> {
+    calculate_path_and_closure::<I>(lines).1
 }
 
-pub fn part_one<I>(lines: I) -> i64
+pub fn calculate_path_and_closure<I>(lines: I) -> (i64, i64)
     where I : Iterator<Item = String> {
     let mut i_s = 0;
     let mut j_s = 0;
     let mut v: Vec<Vec<u8>> = Vec::new();
-    let mut ab: Vec<Vec<i8>> = Vec::new();
-    println!("input");
     for (j,line) in lines.enumerate() {
-        println!("{line}");
         v.push(line.chars().enumerate().map(|(i,c)| 
             if c == 'S' { i_s = i; j_s = j; 0 }
             else if let Some(k) = &DIRECTION.find(c) { *k as u8 }
             else { panic!("Unknown symbol {c}") }
         ).collect());
-        ab.push(vec![0; line.len()]);
     }
     let mut i = i_s;
     let mut j = j_s;
@@ -40,27 +33,23 @@ pub fn part_one<I>(lines: I) -> i64
     let mut len = 0;
     let m = v[0].len() - 1;
     let n = v.len() - 1;
+    let mut ab: Vec<Vec<i8>> = vec![vec![0; m+1]; n+1];
     
     // before we start looping work out a direction to travel in
 
     let mut s = 0;
     if i > 0 && v[j][i - 1] & E != 0 {
-        println!("v[j][i - 1] = {:#06b}",v[j][i - 1]);
         s |= W;
     }
     if j > 0 && v[j - 1][i] & S != 0 {
-        println!("v[j - 1][i] = {:#06b}",v[j - 1][i]);
         s |= N;
     }
     if i < m - 1 && v[j][i + 1] & W != 0 {
-        println!("v[j][i + 1] = {:#06b}",v[j][i + 1]);
         s |= E;
     }
     if j < n - 1 && v[j + 1][i] & N != 0 {
-        println!("v[j + 1][i] = {:#06b}",v[j + 1][i]);
         s |= S;
     }
-    println!("s: {s:#06b}");
     v[j][i] = s;
 
     if s & E != 0 { d = W; }
@@ -68,9 +57,11 @@ pub fn part_one<I>(lines: I) -> i64
     else if s & S != 0 { d = N; }
     else if s & N != 0 { d = S; };
 
-    println!("d: {d:#06b}");
     
     let mut cw = 0i64;
+
+    let mut l : Vec<(usize, usize)> = Vec::new();
+    let mut r : Vec<(usize, usize)> = Vec::new();
 
     // ab is such that
     // -1 if on path
@@ -84,26 +75,25 @@ pub fn part_one<I>(lines: I) -> i64
         let dn = f ^ c;
         if dn == ((f >> 1) | (f << 3)) & MASK { cw += 1; }
         if dn == ((f << 1) | (f >> 3)) & MASK { cw -= 1; }
-        println!("c: {c:#06b}, i: {i}, j: {j}, d: {d:#06b}, f: {f:#06b}, dn: {dn:#06b}");
 
         if d == E || dn == E {
-            if j > 0 { ab[j - 1][i] |= 1; }
-            if j < n - 1 { ab[j + 1][i] |= 2; }
+            if j > 0 { ab[j - 1][i] |= 1; l.push((i, j - 1)); }
+            if j < n { ab[j + 1][i] |= 2; r.push((i, j + 1)); }
         }
 
         if d == N || dn == N {
-            if i > 0 { ab[j][i - 1] |= 1; }
-            if i < m - 1 { ab[j][i + 1] |= 2; }
+            if i > 0 { ab[j][i - 1] |= 1; l.push((i - 1, j)); }
+            if i < m { ab[j][i + 1] |= 2; r.push((i + 1, j)); }
         }
 
         if d == W || dn == W {
-            if j > 0 { ab[j - 1][i] |= 2; }
-            if j < n - 1 { ab[j + 1][i] |= 1; }
+            if j > 0 { ab[j - 1][i] |= 2; r.push((i, j - 1)); }
+            if j < n { ab[j + 1][i] |= 1; l.push((i, j + 1)); }
         }
 
         if d == S || dn == S {
-            if i > 0 { ab[j][i - 1] |= 2; }
-            if i < m - 1 { ab[j][i + 1] |= 1; }
+            if i > 0 { ab[j][i - 1] |= 2; r.push((i - 1, j)); }
+            if i < m { ab[j][i + 1] |= 1; l.push((i + 1, j)); }
         }
 
         d = dn;
@@ -118,20 +108,35 @@ pub fn part_one<I>(lines: I) -> i64
         if i == i_s && j == j_s { break; }
     }
 
-    println!("cw: {cw}");
 
-    println!("parsed:");
-    for j in 0..ab.len() {
-        let s : String = ab[j].iter().map(|&t| 
-            if t < 0 { 'P' }
-            else if t == 1 { 'L' }
-            else if t == 2 { 'R' }
-            else { '.' }
-        ).collect();
-        println!("{s}");
 
+    let mut visited = vec![vec![0; m + 1]; n + 1];
+    let mut inside = if cw < 0 { l } else { r };
+    let mut inside_count = 0;
+    while let Some((i_s,j_s)) = inside.pop() {
+        let mut i = i_s;
+        let mut j = j_s;
+        loop {
+            if ab[j][i] >= 0 && visited[j][i] < 1 {
+                inside_count += 1;
+                visited[j][i] = 1;
+                if i > 0 && ab[j][i - 1] >= 0 && visited[j][i - 1] < 1 {
+                    i -= 1;
+                }
+                else if j > 0 && ab[j - 1][i] >= 0 && visited[j - 1][i] < 1 {
+                    j -= 1;
+                }
+                else if i < m && ab[j][i + 1] >= 0 && visited[j][i + 1] < 1 {
+                    i += 1;
+                }
+                else if j < n && ab[j + 1][i] >= 0 && visited[j + 1][i] < 1 {
+                    j += 1;
+                }
+            }
+            else { break; }
+        }
     }
-    len / 2
+    (len / 2, inside_count)
 }
 
 
